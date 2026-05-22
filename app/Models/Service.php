27c018064c612ec108db7432be1a1a1cad23d4f8
brokiem <milyhqvideo@gmail.com>
2026-05-22 15:06:01 +0800

    Create service model

---
+++ app/Models/Service.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'description', 'status'];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => 'boolean',
+            'price' => 'integer',
+        ];
+    }
+
+    /**
+     * @return HasMany<Subscription, $this>
+     */
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(Subscription::class);
+    }
+}
