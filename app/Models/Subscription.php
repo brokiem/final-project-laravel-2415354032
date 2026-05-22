cddd2948c5640c6d7872f90d3095703da64c46b3
brokiem <milyhqvideo@gmail.com>
2026-05-22 15:11:18 +0800

    Create subscription model

---
+++ app/Models/Subscription.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Subscription extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'service_id',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'date',
+            'end_date' => 'date',
+        ];
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
+}
