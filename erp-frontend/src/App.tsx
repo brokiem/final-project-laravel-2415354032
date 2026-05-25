568b6fa9ecbdbe5cab2256fb44d7d887b2d0cab4
brokiem <milyhqvideo@gmail.com>
2026-05-25 13:33:12 +0800

    Add vite react shadcn project template

---
+++ erp-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { Sidebar } from "@/layouts/Sidebar"
+import { Customers } from "@/pages/Customers"
+import { Services } from "@/pages/Services"
+import { Subscriptions } from "@/pages/Subscriptions"
+
+export function App() {
+    return (
+        <></>
+    )
+}
+
+export default App
